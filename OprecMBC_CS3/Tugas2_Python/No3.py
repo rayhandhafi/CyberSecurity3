878a8c3e4ce7c4b8693f5c9ed2465381523dbcb3
rifanadityaa <rifanadityaputra@gmail.com>
2022-11-06 21:47:35 +0700

    added No3.py in Tugas2_Python

---
+++ OprecMBC_CS3/Tugas2_Python/No3.py
@@ -0,0 +1,14 @@
+Hari=str(input("Masukan Hari : "))
+
+if(Hari=="senin" or Hari=="SENIN"):
+    print("Kamu Nenyee? Aku kasih tau ya Seragam Hari senin adalah Merah")
+elif(Hari=="selasa" or Hari=="SELASA" or Hari=="rabu" or Hari=="RABU"):
+    print("Kamu Nenyee? Aku Kasih Tau Ya Seragam Hari selasa/rabu adalah Putih")
+elif(Hari=="kamis" or Hari=="KAMIS" or Hari=="sabtu" or Hari=="SABTU"):
+    print("Kamu Nenyee? Aku Kasih Tau Ya Seragam Hari kamis/sabtu adalah Bebas")
+elif(Hari=="jumat" or Hari=="JUMAT"):
+    print("Kamu Nenyee? Aku Kasih Tau Ya Seragam Hari jumat adalah Batik")
+elif(Hari=="minggu" or Hari=="MINGGU"):
+    print("Gaada kuliah, hari minggu full senyum")
+else:
+    print("Hari yang anda input salah / tidak ada!")
