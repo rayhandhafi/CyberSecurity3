f47ee1e093b4afc06c147232d60f59969fd6c9a4
rayhandhafi <rayhandhafi21@gmail.com>
2022-11-07 18:21:24 +0700

    updated No3.py parameters

--- OprecMBC_CS3/Tugas2_Python/No3.py
+++ OprecMBC_CS3/Tugas2_Python/No3.py
@@ -1,14 +1,14 @@
 Hari=str(input("Masukan Hari : "))
 
-if(Hari=="senin" or Hari=="SENIN"):
+if(Hari=="senin" or Hari=="SENIN" or Hari=="Senin"):
     print("Kamu Nenyee? Aku kasih tau ya Seragam Hari senin adalah Merah")
-elif(Hari=="selasa" or Hari=="SELASA" or Hari=="rabu" or Hari=="RABU"):
+elif(Hari=="selasa" or Hari=="SELASA" or Hari=="Selasa"or Hari=="rabu" or Hari=="RABU" or Hari=="Rabu"):
     print("Kamu Nenyee? Aku Kasih Tau Ya Seragam Hari selasa/rabu adalah Putih")
-elif(Hari=="kamis" or Hari=="KAMIS" or Hari=="sabtu" or Hari=="SABTU"):
+elif(Hari=="kamis" or Hari=="KAMIS" or Hari=="Kamis"or Hari=="sabtu" or Hari=="SABTU" or Hari=="Sabtu"):
     print("Kamu Nenyee? Aku Kasih Tau Ya Seragam Hari kamis/sabtu adalah Bebas")
-elif(Hari=="jumat" or Hari=="JUMAT"):
+elif(Hari=="jumat" or Hari=="JUMAT"or Hari=="Jumat"):
     print("Kamu Nenyee? Aku Kasih Tau Ya Seragam Hari jumat adalah Batik")
-elif(Hari=="minggu" or Hari=="MINGGU"):
+elif(Hari=="minggu" or Hari=="MINGGU" or Hari=="Minggu"):
     print("Gaada kuliah, hari minggu full senyum")
 else:
     print("Hari yang anda input salah / tidak ada!")
--- OprecMBC_CS3/Tugas2_Python/No4.py
+++ OprecMBC_CS3/Tugas2_Python/No4.py
@@ -1,8 +1,8 @@
-n = int(input("Masukkan angka yang mau dibentuk : "))
+n = int(input("Masukkan nilai n : "))
 t = n
 for i in range(n): 
   for j in range(t):
-      if i ==0 or i == (n-1) or j == 0 or j == (t-1): 
+      if i == 0 or i == (n-1) or j == 0 or j == (t-1): 
         print("*",end ='')
       else :
         print("#",end='')
